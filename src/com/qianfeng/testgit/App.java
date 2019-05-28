a19cc1928e352896692559a274cfdcf481e36d7f
GoodBoylanlan <51064778+GoodBoylanlan@users.noreply.github.com>
2019-05-28 14:23:03 +0800

    Update App.java

--- src/com/qianfeng/testgit/App.java
+++ src/com/qianfeng/testgit/App.java
@@ -3,7 +3,7 @@ package com.qianfeng.testgit;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("项目提交到GitHib上");
+        //System.out.println("项目提交到GitHib上");
 
         System.out.println("修改后的");
     }
